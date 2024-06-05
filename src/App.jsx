569c648d516b990680d777b5bd6f33d4595f1e18
YosaAE24 <cebastin2424@gmail.com>
2024-06-05 22:06:00 +0700

    menambahkan fitur ekstra

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,80 @@
-import React, { useState } from 'react'
+import React, { useState, createContext } from "react";
+import Todos from "./components/Todos";
+import TodoForm from "./components/TodoForm";
 
+export const TodoContext = createContext();
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "Finish Progate React Course",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "Have lunch with Guru Domba",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "Study React with Ninja Ken",
+      completed: false,
+    },
+  ]);
 
-const TodoForm = ({ addTodo }) => {
-     const [title, setTitle] = useState('')
+  console.log(todos);
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        addTodo(title)
-        setTitle('') // Reset title-nya
+  // Definisikan toggleCompleted di sini
+  const toggleCompleted = (todoId) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        todo.completed = !todo.completed;
       }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
 
-      // Definisikan function "handleChangeTitle"
-  const handleChangeTitle = (event) => {
-    setTitle(event.target.value)
-  }
-  
-  // Periksa apakah function "handleChangeTitle" berfungsi
-  console.log(title)
+  // Fungsi untuk menghapus todo berdasarkan id
+  const deleteTodo = (todoId) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== todoId);
+    setTodos(updatedTodos);
+  };
+
+  const addTodo = (todoTitle) => {
+    if (todoTitle === "") {
+      return;
+    }
+
+    const newTodo = {
+      id: todos.length + 1,
+      title: todoTitle,
+      completed: false,
+    };
+
+    const updatedTodos = todos.concat(newTodo);
+    setTodos(updatedTodos);
+  };
 
   return (
-    <div style={styles.container}>
-      <form   onSubmit={(event) => {
-          handleSubmit(event)
-        }}>
-        <input
-          type="text"
-          placeholder="Add your Todo"
-          style={styles.formInput}
-          onChange={(event) => {
-            handleChangeTitle(event)
-          }}
-          value={title}
-        />
-        <button style={styles.button}>Add Todo</button>
-      </form>
-    </div>
-  )
+    <TodoContext.Provider value={{ toggleCompleted, deleteTodo }}>
+      <div style={styles.container}>
+        <h1 style={styles.title}>My Todo List</h1>
+        <TodoForm addTodo={addTodo} />
+        <Todos todos={todos} />
+      </div>
+    </TodoContext.Provider>
+  );
 }
-
 
 const styles = {
   container: {
-    marginBottom: '32px',
+    textAlign: "center",
+    padding: "12px",
   },
-  formInput: {
-    height: '66px',
-    width: '40%',
-    fontSize: '16px',
-    padding: '0 16px',
+  title: {
+    fontSize: "36px",
   },
-  button: {
-    height: '72px',
-    fontSize: '16px',
-  },
-}
+};
 
-
-export default TodoForm
+export default App;
